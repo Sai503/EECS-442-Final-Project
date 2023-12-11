d46197da9a8c60bd96df76f15584727294964dfd
Saipranav Janyavula <34357755+Sai503@users.noreply.github.com>
2023-12-11 10:18:02 -0500

    add personal model

--- matching.py
+++ matching.py
@@ -33,11 +33,11 @@ device = torch.device("cpu")
 
 # using facenet_pytorch for pretrained baseline + mtcnn face extraction
 # constants
-baseImgPath = "2023-09-02_17-39-32_502.jpeg"
+baseImgPath = "George_W_Bush_0001.jpg"
 # baseImgPath = "scanning_images/2023-09-01_11-25-16_677.jpeg"
 searchFolder = "scanning_images/"
 outputFolder = "output_matches/"
-model_image_size = 160 # assume square image
+model_image_size = 112 # assume square image
 pretrained_treshold = 1.3
 selftrained_threshold = 1.3
 
@@ -46,10 +46,80 @@ baseImg = Image.open(baseImgPath)
 # baseImg = torchvision.io.read_image(baseImgPath).to(device)
 
 #load personal model
-#TODO: load model
+class Facenet_NN1(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None)
+        # torch.nn.MaxPool2d(kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+        # torch.nn.LocalResponseNorm(size, alpha=0.0001, beta=0.75, k=1.0)
+        self.conv1 = nn.Conv2d(3, 64, 7,padding=3, stride=2)
+        self.pool1 = nn.MaxPool2d(3, padding=1, stride=2)
+        self.rnorm1 = nn.LocalResponseNorm(64)
 
+        self.conv2a = nn.Conv2d(64, 64, 1, stride=1)
+        self.conv2 = nn.Conv2d(64, 192, 3,padding=1, stride=1)
+        self.rnorm2 = nn.LocalResponseNorm(192)
+        self.pool2 = nn.MaxPool2d(3, padding=1, stride=2)
+
+        self.conv3a = nn.Conv2d(192, 192, 1, stride=1)
+        self.conv3 = nn.Conv2d(192, 384, 3, padding=1, stride=1)
+        self.pool3 = nn.MaxPool2d(3, padding=1, stride=2)
+
+        self.conv4a = nn.Conv2d(384, 384, 1, stride=1)
+        self.conv4 = nn.Conv2d(384, 256, 3, padding=1, stride=1)
+
+        self.conv5a = nn.Conv2d(256, 256, 1, stride=1)
+        self.conv5 = nn.Conv2d(256, 256, 3,padding=1, stride=1)
+
+        self.conv6a = nn.Conv2d(256, 256, 1, stride=1)
+        self.conv6 = nn.Conv2d(256, 256, 3,padding=1, stride=1)
+        self.pool4 = nn.MaxPool2d(3,padding=1, stride=2)
+
+        #todo what is concat layer?
+        self.fc1 = nn.Linear(256*4*4, 128*4*4)
+        # self.maxout1 = nn.AdaptiveMaxPool2d((32,1))
+        self.fc2 = nn.Linear(128 * 4 *4, 128 * 4)
+        # self.maxout2 = nn.MaxPool2d(2)
+
+        self.fc7128 = nn.Linear(128*4,128)
+
+
+
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x1 = self.rnorm1(self.pool1(F.relu(self.conv1(x))))
+        x2 = self.pool2(self.rnorm2(F.relu(self.conv2(self.conv2a(x1)))))
+        x3 = self.pool3(F.relu(self.conv3(self.conv3a(x2))))
+        x4 = F.relu(self.conv4(self.conv4a(x3)))
+        x5 = F.relu(self.conv5(self.conv5a(x4)))
+        x6 = self.pool4(F.relu(self.conv6(self.conv6a(x5))))
+        x6i = torch.flatten(x6, 1)
+        x7 = self.fc1(x6i)
+        # x7i = torch.unflatten(x7, 1, (128, 7, 7))
+        # x8 = self.maxout1(x7i)
+        # x8i = torch.flatten(x8, 1)
+        x9 = self.fc2(x7)
+        # x9i = torch.unflatten(x9, 1, (128, 32))
+        # x10 = self.maxout2(x9i)
+        # x10i = torch.flatten(x10, 1)
+        x11 = self.fc7128(x9)
+        # normalize the output to a unit vector
+        x11 = F.normalize(x11)
+
+        #x7 = self.maxout2(self.fc2(self.maxout1(self.fc1(x6))))
+        # ─Conv2d: 1-16                           [1, 256, 6, 6]            590,080
+        # ─MaxPool2d: 1-17                        [1, 256, 2, 2]            --
+        # return self.fc1(x6)
+        return x11
+        # return self.fc7128(x7)
+        # return x5
+
+model = Facenet_NN1().to(device)
+model.load_state_dict(torch.load("model_final.pth", map_location=torch.device('cpu')))
+model.eval()
 # load pretrained_model
-model = InceptionResnetV1(pretrained='casia-webface').eval().to(device)
+# model = InceptionResnetV1(pretrained='casia-webface').eval().to(device)
 
 # load mtcnn 
 mtcnn = MTCNN(
@@ -79,7 +149,7 @@ print(baseEmbedding.shape)
 
 # iterate through all images in searchFolder with pathlib
 for path in Path(searchFolder).iterdir():
-    if path.is_file() and path.suffix == ".jpeg":
+    if path.is_file() and (path.suffix == ".jpg" or path.suffix == ".jpeg"):
         print(path)
         # load image
         img = Image.open(path)
